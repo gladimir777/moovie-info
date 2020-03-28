e23820b515c8e39b821bf335ffb2784c9b3864bc
Gladimir Jacobsen <gladimir.jacobsen@bovinoapp.com>
2020-03-28 15:52:33 -0300

    header style

---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./header.scss";
+
+function Header(props) {
+  return (
+    <div className="header-wraper">
+      <div className="header-container">
+        <div className="logo"></div>
+        <div className="form-wraper">
+          <div className="form-container">
+            <input type="text" />
+            <button className="btn">GO</button>
+          </div>
+        </div>
+        <div className="login-container">
+          <div className="phone"> (+000) 123 345 653</div>
+          <div>
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Header.propTypes = {};
+
+export default Header;
