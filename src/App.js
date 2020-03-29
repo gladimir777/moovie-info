0c5274474156441245461eaeef423014511b45b5
Gladimir Jacobsen <gladimir.jacobsen@bovinoapp.com>
2020-03-29 18:28:36 -0300

    header design

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 import SubHeader from "./components/subheader/SubHeader";
 import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <SubHeader />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Nav(props) {
+  return <nav className="nav-container">nav</nav>;
+}
+
+Nav.propTypes = {};
+
+export default Nav;
