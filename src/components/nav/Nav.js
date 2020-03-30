8e9996c2821a809b9f4d843b998d4fdfdf10f356
Gladimir Jacobsen <gladimir.jacobsen@bovinoapp.com>
2020-03-29 22:30:03 -0300

    navigation init

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,7 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Nav(props) {
-  return <nav className="nav-container">nav</nav>;
+  return (
+    <nav className="nav-wraper">
+      <ul className="nav-container">
+        <li>
+          <a href="#">home</a>
+        </li>
+        <li>
+          <a href="#">genres</a>
+        </li>
+        <li>
+          <a href="#">tv-series</a>
+        </li>
+        <li>
+          <a href="">news</a>
+        </li>
+        <li>
+          <a href="#">country</a>
+        </li>
+        <li>
+          <a href="#">short-codes</a>
+        </li>
+        <li>
+          <a href="#">a-z list</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 Nav.propTypes = {};
